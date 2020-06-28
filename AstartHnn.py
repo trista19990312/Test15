5b15cffe4b33b46250bbbf8e128c13d9ec4f01f4
ricky958520 <50402066+ricky958520@users.noreply.github.com>
2020-06-28 11:37:11 +0800

    final

--- AstartHnn.py
+++ AstartHnn.py
@@ -264,7 +264,7 @@ if __name__ == '__main__':
                     data = (t.nodes.flatten())
                     path_list.append(data)
                     print(t.nodes)
-                num=len(t.nodes)
+                num=len(t.nodes)-1
                 print('steps:',num)
                 #states_save_auto(path_list)
 
